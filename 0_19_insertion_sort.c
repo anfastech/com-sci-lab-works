e6c112ac430d66e8453f77aeaa2f7d3c49808ac2
Warrior <kpanfas6@gmail.com>
2025-07-01 20:38:38 +0530

    Adding insertion sort : updation

--- 0_19_insertion_sort.c
+++ 0_19_insertion_sort.c
@@ -2,21 +2,21 @@
 #include <conio.h>
 
 int main() {
-    int arr[100], n, i, j, key;
+    int arr[100], n, i, j, temp;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
     printf("Enter the elements of the array: ");
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+    for(i=1;i<n;i++) {
+        for(j=i-1;j>=0;j--) {
+            if(arr[j]>arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1]=temp;
+            }
         }
-        arr[j + 1] = key;
     }
     printf("Sorted array: ");
     for (i = 0; i < n; i++) {
